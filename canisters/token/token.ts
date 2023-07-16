e0a98df7c6ceef30c8186d25bf187cf80e2bcd4f
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-07-17 02:47:43 +0500

    ledger canister moved to root dir

--- canisters/token/token.ts
+++ canisters/token/token.ts
@@ -1,23 +1,135 @@
-
-import { Result, $update, Opt } from 'azle';
 import {
-    BitcoinNetwork,
-    managementCanister,
-    Satoshi
-} from 'azle/canisters/management';
+    ic,
+    match,
+    nat,
+    nat8,
+    Opt,
+    Principal,
+    $query,
+    Record,
+    text,
+    Tuple,
+    $update,
+    Variant,
+    Vec
+} from 'azle';
+import {
+    ICRCAccount,
+    ICRC,
+    ICRCTransferArgs,
+    ICRCTransferError,
+    ICRCValue
+} from 'azle/canisters/icrc';
 
-const BITCOIN_API_CYCLE_COST = 100_000_000n;
+const icrc = new ICRC(
+    Principal.fromText(
+        process.env.ICRC_PRINCIPAL ??
+            ic.trap('process.env.ICRC_PRINCIPAL is undefined')
+    )
+);
+
+$query;
+export async function icrc1_metadata(): Promise<
+    Vec<Tuple<[text, ICRCValue]>>
+> {
+    const result = await icrc.icrc1_metadata().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_name(): Promise<text> {
+    const result = await icrc.icrc1_name().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_symbol(): Promise<text> {
+    const result = await icrc.icrc1_symbol().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_decimals(): Promise<nat8> {
+    const result = await icrc.icrc1_decimals().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_fee(): Promise<nat> {
+    const result = await icrc.icrc1_fee().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_total_supply(): Promise<nat> {
+    const result = await icrc.icrc1_total_supply().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_minting_account(): Promise<Opt<ICRCAccount>> {
+    const result = await icrc.icrc1_minting_account().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_balance_of(account: ICRCAccount): Promise<nat> {
+    const result = await icrc.icrc1_balance_of(account).call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
 
 $update;
-export async function getBalance(
-    address: string
-): Promise<Result<Satoshi, string>> {
-    return await managementCanister
-        .bitcoin_get_balance({
-            address,
-            min_confirmations: Opt.None,
-            network: BitcoinNetwork.Regtest
-        })
-        .cycles(BITCOIN_API_CYCLE_COST)
-        .call();
+export async function icrc1_transfer(
+    transferArgs: ICRCTransferArgs
+): Promise<Variant<{ Ok: nat; Err: ICRCTransferError }>> {
+    const result = await icrc.icrc1_transfer(transferArgs).call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
+}
+
+$query;
+export async function icrc1_supported_standards(): Promise<
+    Vec<Record<{ name: text; url: text }>>
+> {
+    const result = await icrc.icrc1_supported_standards().call();
+
+    return match(result, {
+        Ok: (ok) => ok,
+        Err: (err) => ic.trap(err)
+    });
 }
